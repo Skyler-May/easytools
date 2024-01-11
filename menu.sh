ed974509714a7a740125fa3d3aacfab58f135648
Sam-Mey <meisongyong@gmail.com>
2024-01-11 21:41:34 +0800

    easytools

--- menu.sh
+++ menu.sh
@@ -24,8 +24,8 @@ PURPLE=$(tput setaf 5)
 
 RESET=$(tput sgr0)
 
-# Define menu options
 Define menu options
+# Define menu options
 options=(
     "${BOLD}${GREEN} 站点部署 >> ${RESET}"
     "${BOLD}${GREEN} XrayR [后端] 部署 >> ${RESET}"
@@ -50,10 +50,45 @@ function show_menu() {
     done
 }
 
+# Handle user choice
+function handle_choice() {
+    read -p "${BOLD}${BLUE} 请输入选项编号: ${RESET}" choice
+    # 1-1 菜单
+    case $choice in
+        1)
+            clear  # 清屏
+            echo -e "${BOLD}${RED}你即将进行以下操作...${RESET}\n"
+            while true; do
+                echo -e "${BOLD}${YELLOW}=== 站点相关 === ${RESET}\n"
+                echo -e "${BOLD}${YELLOW} 1. V2Board >> ${RESET}"
+                echo -e "${BOLD}${YELLOW} 2. SSPanel >> ${RESET}"
+                echo -e "${BOLD}${YELLOW} 3. wordpress >> ${RESET}\n"
+                echo -e "${BOLD}${RED} 01. 返回上一级菜单 ${RESET}\n"
+                read -p "${BOLD}${BLUE} 请输入选项编号: ${RESET}" sub_choice
+            done
+        ;;
+        2)
+            clear
+            echo -e "${BOLD}${YELLOW} 选项2 ${RESET}"
+            # 在这里添加选项三的操作
+        ;;
+        3)
+            clear
+            echo -e "${BOLD}${YELLOW} 选项3 ${RESET}"
+            # 在这里添加选项三的操作
+        ;;
+        0)
+            echo "${BOLD}${RED} 谢谢使用，再见！ ${RESET}"
+            exit 0
+        ;;
+        *)
+            echo -e "${BOLD}${RED} 无效选项，请重新选择 ${RESET}"
+        ;;
+    esac
+}
 
-
-# # 主循环
-# while true; do
-#     show_menu
-#     handle_choice
-# done
+# 主循环
+while true; do
+    show_menu
+    handle_choice
+done
