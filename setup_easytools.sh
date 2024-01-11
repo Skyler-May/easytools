c421386e682b5487d5260df48d0bc3594bf84d5c
Sam-Mey <meisongyong@gmail.com>
2024-01-12 02:59:37 +0800

    easytools

--- setup_easytools.sh
+++ setup_easytools.sh
@@ -6,11 +6,11 @@ git clone https://github.com/Sam-Mey/easytools.git
 # Find all .sh files and chmod +x
 find easytools -name "*.sh" -exec chmod +x {} \;
 
-# Run 'et' command (make sure 'et' is set up before this point)
-et
-
 # Create a symbolic link for menu.sh
 ln -s "/root/easytools/menu.sh" "/usr/local/bin/et"
+
+# Run 'et' command (make sure 'et' is set up before this point)
+et
 
 # Delete script after Run 'et'
 find / -name "setup_easytools.sh" -exec rm {} \;
