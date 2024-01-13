c2ab662696258a1a5901f1a4d565bfd15839e974
Sam-Mey <meisongyong@gmail.com>
2024-01-13 15:40:49 +0800

    easytools

--- config.sh
+++ config.sh
@@ -1,34 +1,10 @@
-
-
-# # 切换到脚本所在目录
-# cd "$(dirname "${BASH_SOURCE[0]}")"
-
-# # 保存脚本所在目录的路径
-# SCRIPT_DIR="/root/easytools"
-
-# # 定义其他路径
-# MENU_SCRIPT_PATH="$SCRIPT_DIR/menu.sh"
-# WEBSITE_MENU_SCRIPT_PATH="$SCRIPT_DIR/website/website_menu.sh"
-# UNINSTALL_SCRIPT_PATH="$SCRIPT_DIR/uninstall.sh"
-# UPDATA_SCRIPT_PATH="$SCRIPT_DIR/updata.sh"
-# # 添加其他路径...
-
-# # 其他配置...
-
-# # 打印调试信息
-# echo "SCRIPT_DIR: $SCRIPT_DIR"
-# echo "MENU_SCRIPT_PATH: $MENU_SCRIPT_PATH"
-# echo "WEBSITE_MENU_SCRIPT_PATH: $WEBSITE_MENU_SCRIPT_PATH"
-# echo "UNINSTALL_SCRIPT_PATH: $UNINSTALL_SCRIPT_PATH"
-# echo "UPDATA_SCRIPT_PATH: $UPDATA_SCRIPT_PATH"
-
-#!/bin/bash
+!/bin/bash
 
 # 切换到脚本所在目录
-cd "$(dirname "$0")"
+cd "$(dirname "${BASH_SOURCE[0]}")"
 
 # 保存脚本所在目录的路径
-SCRIPT_DIR="$(pwd)"
+SCRIPT_DIR="/root/easytools"
 
 # 定义其他路径
 MENU_SCRIPT_PATH="$SCRIPT_DIR/menu.sh"
--- updata.sh
+++ updata.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+# Clone the repository
+git clone https://github.com/Sam-Mey/easytools.git
+
+# Find all .sh files and chmod +x
+find easytools -name "*.sh" -exec chmod +x {} \;
+
+# Create a symbolic link for menu.sh
+ln -s "/root/easytools/menu.sh" "/usr/local/bin/et"
+
+# Run 'et' command (make sure 'et' is set up before this point)
+et
+
+# Wait for a while to ensure 'et' has finished
+sleep 5
+
+# Delete script after Run 'et'
+find / -name "setup_easytools.sh" -exec rm {} \;
