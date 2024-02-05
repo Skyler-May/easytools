bfe613123725d1f927aefa4dffc2cfd53b45a8d1
Sam-Mey <meisongyong@gmail.com>
2024-02-05 15:12:38 +0800

    easytools

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cd /
 git clone https://github.com/Sam-Mey/easytools.git
 
 # Find all .sh files and chmod +x
-find easytools -name "*.sh" -exec chmod +x {} \;
+find /easytools -name "*.sh" -exec chmod +x {} \;
 
 # Create a symbolic link for menu.sh
 ln -s "/easytools/menu.sh" "/usr/local/bin/et"
