06b85dfcaf4013a1fa944f931ae15de19f36c671
Sam-Mey <meisongyong@gmail.com>
2024-02-05 19:07:51 +0800

    easytools

--- docker-lnmp/ubuntu-debian/arm64/delete_container_network.sh
+++ docker-lnmp/ubuntu-debian/arm64/delete_container_network.sh
@@ -4,10 +4,10 @@ echo "停止所有容器..."
 docker stop $(docker ps -aq)
 
 echo "删除所有容器..."
-docker rm $(docker ps -aq)
+docker rm -f $(docker ps -aq)
 
 echo "删除所有镜像..."
-docker rmi -y $(docker images -aq)
+docker rmi -f $(docker images -aq)
 
 echo "清理网络..."
-docker network prune -y
+docker network prune -f
