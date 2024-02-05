6eb826c9a6fdd5022d519ba184d6a084cd028fd1
Sam-Mey <meisongyong@gmail.com>
2024-02-06 04:01:38 +0800

    easytools

--- website/sspanel/ubuntu/install_sspanel.sh
+++ website/sspanel/ubuntu/install_sspanel.sh
@@ -7,17 +7,21 @@ check_port() {
     port=$1
     if timeout 1 bash -c "</dev/tcp/$host/$port" >/dev/null 2>&1; then
         echo "Port $port is open"
-        "$SSL_MENU_SCRIPT_PATH"
+        # 直接使用已配置的变量
+        $SSL_MENU_SCRIPT_PATH
     else
         echo "Port $port is closed"
         read -p "Do you want to open port $port? (y/n): " choice
         if [ "$choice" == "y" ]; then
-            # 在这里添加打开端口的命令，例如使用防火墙工具如ufw
+            # 在这里添加打开端口的命令，根据你的系统和防火墙工具来定制
             # 示例: ufw allow $port
             echo "Opening port $port..."
         fi
     fi
 }
 
+# 检查80端口
 check_port 80
+
+# 检查443端口
 check_port 443
