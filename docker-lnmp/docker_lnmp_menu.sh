630d698446dc0c483ec85da0a71fa4571ba48ee8
Sam-Mey <meisongyong@gmail.com>
2024-02-06 03:22:11 +0800

    easytools

--- docker-lnmp/docker_lnmp_menu.sh
+++ docker-lnmp/docker_lnmp_menu.sh
@@ -67,14 +67,21 @@ function handle_choice() {
     # 1-1 菜单
     case $choice in
         1)
-            # 检查是否安装了Docker
+            # 检查是否安装了 Docker
             if command -v docker &> /dev/null; then
                 clear
-                echo -e "${BOLD}${YELLOW} 安装 ${RESET}"
-                "$INSTALL_DOCKER_LNMP_SCRIPT_PATH"
+                echo -e "${BOLD}${YELLOW} Docker 已安装，可以继续执行脚本 ${RESET}"
             else
-                echo -e "${RED}未安装 Docker !!! 即将安装 Docker...${RESET}"
+                echo -e "${RED}未安装 Docker !!! 即将为您安装 Docker...${RESET}"
                 "$DOCKER_SCRIPT_PATH"
+                
+                # 再次检查 Docker 是否安装成功
+                if command -v docker &> /dev/null; then
+                    echo -e "${BOLD}${YELLOW} Docker 安装成功，可以继续执行脚本 ${RESET}"
+                    "$INSTALL_DOCKER_LNMP_SCRIPT_PATH"
+                else
+                    echo -e "${RED}Docker 安装失败，请检查安装过程中的错误。无法继续执行 LNMP 脚本.${RESET}"
+                fi
             fi
         ;;
         2)
