305bb790ff5fd7dbcc300e16d69253afdb3fa771
Sam-Mey <meisongyong@gmail.com>
2024-03-10 20:12:35 +0800

    easytools

--- warp/warp_install.sh
+++ warp/warp_install.sh
@@ -1,3 +1,5 @@
-#!/bash/bin
+#!/bin/bash
 
-wget -N https://gitlab.com/fscarmen/warp/-/raw/main/menu.sh && bash menu.sh [option] [lisence/url/token]
+wget -N https://gitlab.com/fscarmen/warp/-/raw/main/menu.sh
+chmod +x menu.sh
+bash menu.sh [option] [lisence/url/token]
