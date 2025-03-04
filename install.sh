835f221891082546a1bc2cc1246b71cff6c01afc
Skyler-May <meisongyong@gmail.com>
2025-03-04 16:44:50 +0800

    Initial commit

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ root_need
 cd /
 
 # Clone the repository
-git clone https://github.com/Sam-Mey/easytools.git
+git clone https://github.com/Skyler-May/easytools.git
 
 # Find all .sh files and chmod +x
 find /easytools -name "*.sh" -exec chmod +x {} \;
